41bfd9bbc016dd4f3ea346034f21e7efe9fcec64
Jairo QA <jairoam85@gmail.com>
2023-03-30 15:04:51 -0300

    Foi implementado lógica para cenários inválidos no cadastro do entregador

--- features/step_definitions/my_cadastro/cadastro_invalido_step.rb
+++ features/step_definitions/my_cadastro/cadastro_invalido_step.rb
@@ -1,13 +1,9 @@
 Dado('preencher os campos com {string},{string},{string},{string},{string}') do |nome, cpf, email, cep, numero|
-    @deliver_page.cadastro_sections.preencher_form_com_dados_informados(
-        UserData.get(nome),
-        UserData.get(cpf),
-        UserData.get(email),
-        UserData.get(cep),
-        UserData.get(numero)        
-      )
-  end
+    @deliver_page.form_deliver.preencher_form_com_dados_informados(nome, cpf, email, cep, numero)    
+end
   
-  Então('deve exibir no campo a mensagem de erro {string}') do |msg_falha_campo|
+Então('deve exibir no campo a mensagem de erro {string}') do |msg_falha_campo|
+    # @deliver_page.wait_until_message_erro_visible  
     expect(@deliver_page).to have_content msg_falha_campo
-  end
+    # sleep 5
+end
